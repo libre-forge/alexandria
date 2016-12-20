21071795f0f2a6ef7720ac3eed4cc87a5de5975a
Mario Garcia <mario.ggar@gmail.com>
2016-12-20 14:35:04 +0100

    Add default creation date to tables

--- resources/migrations/0000.init.sql
+++ resources/migrations/0000.init.sql
@@ -1,39 +1,41 @@
 CREATE TABLE liber (
-  id uuid,
+  id uuid PRIMARY KEY,
   name text,
   email text,
   password text,
-  created_at timestamp);
+  created_at timestamp DEFAULT CURRENT_DATE);
 
 INSERT INTO liber (id, email, password, name) VALUES ('588034cb-4c84-4f36-abcf-fa511aba0de3', 'john.doe@gmail.com', 'password1234', 'John Doe');
 INSERT INTO liber (id, email, password, name) VALUES ('e02627de-2482-42ed-ab72-81ef4d376855', 'alexa.doe@gmail.com', 'password1234', 'Alexa');
 INSERT INTO liber (id, email, password, name) VALUES ('2adb5684-4b20-45df-84f2-c4971dc42921', 'preston.doe@gmail.com', 'password1234', 'Preston McMillan');
+INSERT INTO liber (id, email, password, name) VALUES ('ee502a6b-2e9f-4b54-babf-97ada2981e9d', 'libreforge.user@gmail.com', 'password1234', 'Libre McMillan');
 
 CREATE TABLE course (
-  id uuid,
+  id uuid PRIMARY KEY,
   title text,
-  created_at timestamp,
+  description text,
+  created_at timestamp DEFAULT CURRENT_DATE,
   created_by uuid);
 
 CREATE TABLE theme (
-  id uuid,
+  id uuid PRIMARY KEY,
   title text,
   course uuid,
-  created_at timestamp,
+  created_at timestamp DEFAULT CURRENT_DATE,
   created_by uuid);
 
 CREATE TABLE resource (
-  id uuid,
+  id uuid PRIMARY KEY,
   title text,
   description text,
   theme uuid,
-  created_at timestamp,
+  created_at timestamp DEFAULT CURRENT_DATE,
   created_by uuid);
 
 CREATE TABLE comment (
-  id uuid,
+  id uuid PRIMARY KEY,
   content text,
   theme uuid,
   course uuid,
-  created_at timestamp,
+  created_at timestamp DEFAULT CURRENT_DATE,
   created_by uuid);
